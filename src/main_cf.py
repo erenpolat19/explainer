9bf2286e9f2b3dd3439e88fa626c4425241ede29
Eren Polat <eren@ucsb.edu>
2024-10-09 16:52:36 -0700

    cf main partial

--- src/main_cf.py
+++ src/main_cf.py
@@ -1,6 +1,8 @@
 from model.models import *
 from utils import *
 
+from torch_geometric.utils import to_dense_adj
+
 sys.path.append('../')
 
 parser = argparse.ArgumentParser(description='')
@@ -10,6 +12,9 @@ parser.add_argument('--epochs', type=int, default=2000, metavar='N',
                     help='number of epochs to train (default: 2000)')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
+
+parser.add_argument('--vae_cf_epoch', type=int, default=50, metavar='N',
+                    help='epochs to train without cf loss')
 
 parser.add_argument('--trn_rate', type=float, default=0.6, help='training data ratio')
 parser.add_argument('--tst_rate', type=float, default=0.2, help='test data ratio')
@@ -35,22 +40,42 @@ args = parser.parse_args()
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 
-def loss_cf(reconstr_a, orig_a, y_cf, z_mu, z_logvar, clf_model):
+def loss(reconstr_a, orig_a, orig_x, y_pred, y_cf, z_mu, z_logvar, alpha):
+    #reconstr loss
+    reconst_loss = F.binary_cross_entropy(reconstr_a, orig_a)
 
+    #kl loss, maybe add the learned prior later etc.
+    kl_loss = -0.5 * torch.sum(1 + z_logvar - z_mu.pow(2) - z_logvar.exp())
 
-def loss_f(pred, target, mask, reg_coefs):
-    
-    scale = 0.99
-    mask = mask*(2*scale-1.0)+(1.0-scale)
-    
-    cce_loss = F.cross_entropy(pred, target)
-    size_loss = torch.sum(mask) * reg_coefs[0]
-    mask_ent_reg = -mask * torch.log(mask) - (1 - mask) * torch.log(1 - mask)
-    mask_ent_loss = reg_coefs[1] * torch.mean(mask_ent_reg)
-    
-    return cce_loss + size_loss + mask_ent_loss    
+    #cf loss
+    cf_loss = F.binary_cross_entropy(y_pred, y_cf)
 
-def train(clf_model, cf_explainer, optimizer_cf, train_loader, val_loader, test_loader, device, args, temp=(5.0, 2.0)):
+    return reconst_loss + kl_loss + alpha * cf_loss, reconst_loss, kl_loss, cf_loss
+
+def get_counterfactual(inputs, cf_explainer, clf_model, data, params, y_cf, beta=1):
+    x, edge_index, edge_weights = inputs
+    reconstr_edge_index, z_mu, z_logvar = cf_explainer(inputs, beta=beta, y_target=y_cf, batch=data.batch)
+    reconstr_a = to_dense_adj(reconstr_edge_index, data.batch, max_num_nodes=params['num_nodes'])
+    orig_a = to_dense_adj(edge_index, data.batch, max_num_nodes=params['num_nodes'])
+    y_pred = clf_model(x, reconstr_edge_index, edge_weights=None, batch=data.batch)
+
+    return reconstr_a, reconstr_edge_index, orig_a, y_pred, z_mu, z_logvar
+
+def test(cf_explainer, clf_model, loader, params, device):
+    cf_explainer.eval()
+    total_loss_cf = 0
+    for data in loader:  # Iterate in batches over the training/test dataset.
+        with torch.no_grad():
+            x, edge_index, y_target  = data.x.to(device), data.edge_index.to(device), data.y.to(device)
+            y_cf = 1 - y_target
+            reconstr_a, reconstr_edge_index, orig_a, y_pred, z_mu, z_logvar = get_counterfactual((x, edge_index, None), cf_explainer, clf_model, data, params, y_cf, beta=1)
+            
+            loss_total, loss_reconstr, loss_kl, loss_cf = loss(reconstr_a, orig_a, y_pred, y_cf, z_mu, z_logvar, alpha=1)
+            total_loss_cf += loss_total.item() 
+
+    return total_loss_cf
+
+def train(clf_model, cf_explainer, optimizer_cf, train_loader, val_loader, test_loader, device, args, params, temp=(5.0, 2.0)):
     temp_schedule = lambda e: temp[0] * ((temp[1] / temp[0]) ** (e / args.epochs))
     for epoch in range(args.epochs):
         cf_explainer.train()
@@ -59,16 +84,23 @@ def train(clf_model, cf_explainer, optimizer_cf, train_loader, val_loader, test_
         
         for data in train_loader:
             x, edge_index, y_target = data.x.to(device), data.edge_index.to(device), data.y.to(device)
-
+            y_cf = 1 - y_target
             optimizer_cf.zero_grad()
 
-            cf_explainer.
-            loss = loss_cf(, y_target, expl_mask, reg_coefs)
+            #mask (or edge weight) is none atm
+            reconstr_a, reconstr_edge_index, orig_a, y_pred, z_mu, z_logvar = get_counterfactual((x, edge_index, None), cf_explainer, clf_model, data, params, y_cf, beta=1)
+            
+            
+            
+            alpha = 1
+            if epoch < args.vae_cf_epoch: #check this later
+                alpha = 0
 
-            loss.backward()
+            loss_total, loss_reconstr, loss_kl, loss_cf = loss(reconstr_a, orig_a, y_pred, y_cf, z_mu, z_logvar, alpha)
+            loss_total.backward()
             optimizer_cf.step()
 
-            total_loss_f += loss.item(
+            total_loss_cf += loss_total.item()
 
 def run(args):
     device = "cpu"
@@ -76,30 +108,39 @@ def run(args):
     load data for train, val, test
     """
     #dataset_name = args.dataset
-    dataset_name = 'BA-2motif'
+    dataset_name = 'BA-2motif-this-one-works'
     data = preprocess_ba_2motifs(dataset_name)
     train_loader, val_loader, test_loader = get_dataloaders(data, batch_size=64, val_split=0.1, test_split=0.1)
 
-    x_dim = 10
-    h_dim = 20
-    z_dim = h_dim
-    num_classes = 2
-    num_nodes = train_loader[0].x.shape[0]
+    # x_dim = 10
+    # h_dim = 20
+    # z_dim = h_dim
+    # num_classes = 2
+    # num_nodes = train_loader[0].x.shape[0]
+    for data in train_loader:
+        x = data.x  
+        num_nodes = x.shape[0]
+        break 
+
+    params = {'x_dim': 10, 'h_dim': args.h_dim, 'z_dim': args.h_dim, 'num_classes': 2, 
+              'num_nodes': num_nodes}
 
     # embedder
-    clf_model = GCN(x_dim, num_classes).to(device)              # load clf
-    checkpoint = torch.load('clf.pth')
+    clf_model = GCN(params['x_dim'], params['num_classes'], pooling='mean').to(device)              # load clf
+    checkpoint = torch.load('clf-good.pth')
     clf_model.load_state_dict(checkpoint)
     clf_model.eval()                                                              
 
-    with open('pretrained/pg_pretrained.pkl', 'rb') as f:
-        factual_explainer = pkl.load(f)
-
-    cf_explainer = GNN_MLP_VariationalAutoEncoder(x_dim, num_nodes * num_nodes).to(device)
+    cf_explainer = GNN_MLP_VariationalAutoEncoder(params['x_dim'], params['num_nodes'] * params['num_nodes']).to(device)
 
     optimizer_cf = Adam(cf_explainer.parameters(), lr=args.lr, weight_decay=args.weight_decay).to(device)
     
-    train(clf_model, factual_explainer, optimizer_cf, train_loader, val_loader, test_loader, device, args)
+    train(clf_model, cf_explainer, optimizer_cf, train_loader, val_loader, test_loader, device, args, params)
+
+    val_loss = test(cf_explainer, clf_model, val_loader, params, device)
+    test_loss = test(cf_explainer, clf_model, test_loader, params, device)
+
+    print("Val loss: ", val_loss, "Test loss: ", test_loss)
     
 run(args)
 
--- src/model/models.py
+++ src/model/models.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 from torch.nn import ReLU, Linear
+from numbers import Number
 import torch.nn.init as init
-from torch_geometric.nn import GCNConv, global_max_pool, global_mean_pool, global_add_pool
+from torch_geometric.nn import GCNConv, global_max_pool, global_mean_pool, global_add_pool, GraphConv
 from model.gcn import *
 import torch.nn.functional as F
 
@@ -37,10 +38,11 @@ class GraphEncoder(nn.Module):
         elif type == 'both':
             out1 = global_max_pool(embed, batch)
             out2 = global_mean_pool(embed, batch)
+            out = torch.cat([out1, out2], dim=-1)     
         return out
     
     def forward(self, x, edge_index, edge_weights, y_target, batch): 
-        graph_emb = self.graph_pooling(self.embedder.embedding(x, edge_index, edge_weights=edge_weights), )
+        graph_emb = self.graph_pooling(self.embedder.embedding(x, edge_index, edge_weights=edge_weights), batch, type='mean')
         
         z_mu = self.encoder_mean(torch.cat((graph_emb, y_target), dim=1))
         z_logvar = self.encoder_var(torch.cat((graph_emb, y_target), dim=1))
@@ -98,7 +100,7 @@ class GNN_MLP_VariationalAutoEncoder(nn.Module):
         self.fc_mu = nn.Linear(256, hidden_dim)
         self.fc_logvar = nn.Linear(256, hidden_dim)
 
-    def encode(self, inputs):
+    def encode(self, inputs, y_target):
         x, edge_index, edge_weight = inputs
     
         out1 = self.conv1(x, edge_index, edge_weight=edge_weight)
@@ -118,6 +120,9 @@ class GNN_MLP_VariationalAutoEncoder(nn.Module):
         mu = self.fc_mu(input_lin)
         logvar = self.fc_logvar(input_lin)
 
+        mu = torch.cat((mu, y_target), dim=1)
+        logvar = torch.cat((logvar, y_target), dim=1)
+
         return mu, logvar
 
     def reparameterize(self, mu, logvar):
@@ -129,9 +134,9 @@ class GNN_MLP_VariationalAutoEncoder(nn.Module):
     def decode(self, z):
         return torch.sigmoid(self.decoder(z))
 
-    def forward(self,inputs,beta, batch=None):
+    def forward(self,inputs,beta, y_target, batch=None):
         
-        mu, logvar = self.encode(inputs)
+        mu, logvar = self.encode(inputs, y_target)
         z = self.reparameterize(mu, beta * logvar)
         
         if batch is None:
@@ -146,3 +151,187 @@ class GNN_MLP_VariationalAutoEncoder(nn.Module):
         recon_x = self.decode(reduce_z)
 
         return recon_x, mu, logvar
+    
+
+#CLEAR MLP
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, n_layers, activation='none', slope=.1, device='cpu'):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.n_layers = n_layers
+        self.device = device
+        if isinstance(hidden_dim, Number):
+            self.hidden_dim = [hidden_dim] * (self.n_layers - 1)
+        elif isinstance(hidden_dim, list):
+            self.hidden_dim = hidden_dim
+        else:
+            raise ValueError('Wrong argument type for hidden_dim: {}'.format(hidden_dim))
+
+        if isinstance(activation, str):
+            self.activation = [activation] * (self.n_layers - 1)
+        elif isinstance(activation, list):
+            self.hidden_dim = activation
+        else:
+            raise ValueError('Wrong argument type for activation: {}'.format(activation))
+
+        self._act_f = []
+        for act in self.activation:
+            if act == 'lrelu':
+                self._act_f.append(lambda x: F.leaky_relu(x, negative_slope=slope))
+            elif act == 'xtanh':
+                self._act_f.append(lambda x: self.xtanh(x, alpha=slope))
+            elif act == 'sigmoid':
+                self._act_f.append(F.sigmoid)
+            elif act == 'none':
+                self._act_f.append(lambda x: x)
+            else:
+                ValueError('Incorrect activation: {}'.format(act))
+
+        if self.n_layers == 1:
+            _fc_list = [nn.Linear(self.input_dim, self.output_dim)]
+        else:
+            _fc_list = [nn.Linear(self.input_dim, self.hidden_dim[0])]
+            for i in range(1, self.n_layers - 1):
+                _fc_list.append(nn.Linear(self.hidden_dim[i - 1], self.hidden_dim[i]))
+            _fc_list.append(nn.Linear(self.hidden_dim[self.n_layers - 2], self.output_dim))
+        self.fc = nn.ModuleList(_fc_list)
+        self.to(self.device)
+
+    @staticmethod
+    def xtanh(x, alpha=.1):
+        """tanh function plus an additional linear term"""
+        return x.tanh() + alpha * x
+
+    def forward(self, x):
+        h = x
+        for c in range(self.n_layers):
+            if c == self.n_layers - 1:
+                h = self.fc[c](h)
+            else:
+                h = self._act_f[c](self.fc[c](h))
+        return h
+    
+
+ #Adapted from clear   
+class GraphCFE(nn.Module):
+    def __init__(self, init_params, args):
+        super(GraphCFE, self).__init__()
+        self.vae_type = init_params['vae_type']  # graphVAE
+        self.x_dim = init_params['x_dim']
+        self.h_dim = args.dim_h
+        self.z_dim = args.dim_z
+        self.u_dim = 1 # init_params['u_dim']
+        self.dropout = args.dropout
+        self.max_num_nodes = init_params['max_num_nodes']
+        self.encoder_type = 'gcn'
+        self.graph_pool_type = 'mean'
+        self.disable_u = args.disable_u
+
+        if self.disable_u:
+            self.u_dim = 0
+            print('disable u!')
+        if self.encoder_type == 'gcn':
+            self.graph_model = GCNConv(self.x_dim, self.h_dim)
+        elif self.encoder_type == 'graphConv':
+            self.graph_model = GraphConv(self.x_dim, self.h_dim)
+
+        # prior
+        self.prior_mean = MLP(self.u_dim, self.z_dim, self.h_dim, n_layers=1, activation='none', slope=.1, device=device)
+        self.prior_var = nn.Sequential(MLP(self.u_dim, self.z_dim, self.h_dim, n_layers=1, activation='none', slope=.1, device=device), nn.Sigmoid())
+
+        # encoder
+        self.encoder_mean = nn.Sequential(nn.Linear(self.h_dim + self.u_dim + 1, self.z_dim), nn.BatchNorm1d(self.z_dim), nn.ReLU())
+        self.encoder_var = nn.Sequential(nn.Linear(self.h_dim + self.u_dim + 1, self.z_dim), nn.BatchNorm1d(self.z_dim), nn.ReLU(), nn.Sigmoid())
+
+        # decoder
+        self.decoder_x = nn.Sequential(nn.Linear(self.z_dim + 1, self.h_dim), nn.BatchNorm1d(self.h_dim), nn.Dropout(self.dropout), nn.ReLU(),
+                                       nn.Linear(self.h_dim, self.h_dim), nn.BatchNorm1d(self.h_dim), nn.Dropout(self.dropout), nn.ReLU(),
+                                       nn.Linear(self.h_dim, self.max_num_nodes*self.x_dim))
+        self.decoder_a = nn.Sequential(nn.Linear(self.z_dim + 1, self.h_dim), nn.BatchNorm1d(self.h_dim), nn.Dropout(self.dropout), nn.ReLU(),
+                                       nn.Linear(self.h_dim, self.h_dim), nn.BatchNorm1d(self.h_dim), nn.Dropout(self.dropout), nn.ReLU(),
+                                       nn.Linear(self.h_dim, self.max_num_nodes*self.max_num_nodes), nn.Sigmoid())
+        self.graph_norm = nn.BatchNorm1d(self.h_dim)
+
+    def encoder(self, features, u, adj, y_cf):
+        # Q(Z|X,U,A,Y^CF)
+        # input: x, u, A, y^cf
+        # output: z
+        graph_rep = self.graph_model(features, adj)  # n x num_node x h_dim
+        graph_rep = self.graph_pooling(graph_rep, self.graph_pool_type)  # n x h_dim
+        #graph_rep = self.graph_norm(graph_rep)
+
+        if self.disable_u:
+            z_mu = self.encoder_mean(torch.cat((graph_rep, y_cf), dim=1))
+            z_logvar = self.encoder_var(torch.cat((graph_rep, y_cf), dim=1))
+        else:
+            z_mu = self.encoder_mean(torch.cat((graph_rep, u, y_cf), dim=1))
+            z_logvar = self.encoder_var(torch.cat((graph_rep, u, y_cf), dim=1))
+
+        return z_mu, z_logvar
+
+    def get_represent(self, features, u, adj, y_cf):
+        u_onehot = u
+        # encoder
+        z_mu, z_logvar = self.encoder(features, u_onehot, adj, y_cf)
+
+        return z_mu, z_logvar
+
+    def decoder(self, z, y_cf, u):
+        if self.disable_u:
+            adj_reconst = self.decoder_a(torch.cat((z, y_cf), dim=1)).view(-1, self.max_num_nodes,
+                                                                              self.max_num_nodes)
+        else:
+            adj_reconst = self.decoder_a(torch.cat((z, y_cf), dim=1)).view(-1, self.max_num_nodes, self.max_num_nodes)
+
+        features_reconst = self.decoder_x(torch.cat((z, y_cf), dim=1)).view(-1, self.max_num_nodes, self.x_dim)
+        return features_reconst, adj_reconst
+
+    def graph_pooling(self, x, type='mean'):
+        if type == 'max':
+            out, _ = torch.max(x, dim=1, keepdim=False)
+        elif type == 'sum':
+            out = torch.sum(x, dim=1, keepdim=False)
+        elif type == 'mean':
+            out = torch.sum(x, dim=1, keepdim=False)
+        return out
+
+    def prior_params(self, u):  # P(Z|U)
+        if self.disable_u:
+            z_u_mu = torch.zeros((len(u),self.h_dim)).to(device)
+            z_u_logvar = torch.ones((len(u),self.h_dim)).to(device)
+        else:
+            z_u_logvar = self.prior_var(u)
+            z_u_mu = self.prior_mean(u)
+        return z_u_mu, z_u_logvar
+
+    def reparameterize(self, mu, logvar):
+        '''
+        compute z = mu + std * epsilon
+        '''
+        if self.training:
+            # compute the standard deviation from logvar
+            std = torch.exp(0.5 * logvar)
+            # sample epsilon from a normal distribution with mean 0 and
+            # variance 1
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def score(self):
+        return
+
+    def forward(self, features, u, adj, y_cf):
+        u_onehot = u
+
+        z_u_mu, z_u_logvar = self.prior_params(u_onehot)
+        # encoder
+        z_mu, z_logvar = self.encoder(features, u_onehot, adj, y_cf)
+        # reparameterize
+        z_sample = self.reparameterize(z_mu, z_logvar)
+        # decoder
+        features_reconst, adj_reconst = self.decoder(z_sample, y_cf, u_onehot)
+
+        return {'z_mu': z_mu, 'z_logvar': z_logvar, 'adj_permuted': adj, 'features_permuted': features,
+                'adj_reconst': adj_reconst, 'features_reconst': features_reconst, 'z_u_mu': z_u_mu, 'z_u_logvar': z_u_logvar}
